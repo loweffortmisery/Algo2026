e64acbb557000e78924f5798c30c737f2f917dee
loweffortmisery <189833560+loweffortmisery@users.noreply.github.com>
2026-02-22 23:17:04 +0200

    Implement open-addressed hash-table as part of the task 6.3

--- Homework/3/t06_03/user.py
+++ Homework/3/t06_03/user.py
@@ -4,19 +4,189 @@
 Бібліотека може містити кілька книг одного автора.
 """
 
+EMPTY = "EMPTY"
+DELETED = "DELETED"
+
+
+
+
+
+def is_prime(n: int) -> bool:
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+class Hash_table:
+    def __init__(self):
+        self.size = 11
+        self.current_size = 0
+        self.keys = [None]
+        self.values = [None]
+        self.resize()
+
+    def resize(self):
+        self.current_size = 0
+        self.keys = [EMPTY for _ in range(self.size)] 
+        self.values = [EMPTY for _ in range(self.size)] 
+
+    def hash(self, S: str):
+        N = 31
+        M = 100007
+        h = 0
+        for i in range(len(S)):
+            h = h * N + ord(S[i])
+        return (h % M) % self.size
+
+    def rehash(self):
+        self.size = self.size * 2 + 1
+        while not is_prime(self.size):
+            self.size += 2
+        _keys = self.keys
+        _values = self.values
+        self.resize()
+        for i in range(len(_keys)):
+            if _keys[i] not in (EMPTY, DELETED):
+                self.put(_keys[i], _values[i])
+
+    def put(self, key, value):
+        if self.current_size > 0.7 * self.size:
+            self.rehash()
+        current = self.hash(key)
+        j = -1
+        while self.keys[current] is not EMPTY:
+            if self.keys[current] == key:
+                self.values[current] = value
+                return
+            if ((j == -1) and (self.keys[current] == DELETED)):
+                j = current
+            current = (current + 1) % self.size
+        if j == -1:
+            j = current
+            self.current_size += 1
+
+        self.keys[j] = key
+        self.values[j] = value 
+
+    def get(self, key):
+        current = self.hash(key)
+        while self.keys[current] is not EMPTY:
+            if self.keys[current] == key:
+                return self.values[current]
+            current = (current + 1) % self.size
+        return None
+
+    def remove(self, key):
+        i = self.hash(key)
+        while self.keys[i] is not EMPTY:
+            if self.keys[i] == key:
+                self.keys[i] = DELETED
+                self.values[i] = DELETED
+                return None
+            i = (i + 1) % self.size
+        return None
+
+
+class table_keys_titles(Hash_table):
+    def remove_title(self, title):
+        self.remove(title)
+
+    def add_title(self, title):
+        self.put(title, True)
+
+    def check_for_title(self, title):
+        return (self.get(title) == True)
+
+    def get_all_titles(self):
+        titles = []
+        for i in range(self.size):
+            if self.keys[i] not in (EMPTY, DELETED):
+                titles.append(self.keys[i])
+        return titles
+
+    def get_all_sorted(self):
+        return sorted(self.get_all_titles())
+
+
+class table_keys_authors(Hash_table):
+    def resize(self):
+        self.current_size = 0
+        self.keys = [EMPTY for _ in range(self.size)] 
+        self.values = [EMPTY for _ in range(self.size)] 
+    
+    def rehash(self):
+        self.size = self.size * 2 + 1
+        while not is_prime(self.size):
+            self.size += 2
+        _keys = self.keys
+        _values = self.values
+        self.resize()
+        for i in range(len(_keys)):
+            if _keys[i] is not EMPTY:
+                author = _keys[i]
+                _titles = _values[i].get_all_titles()
+                if len(_titles) == 0:
+                    continue
+                self.put(author, _titles[0])
+                new_titles = self.get(author)
+                for j in range(1, len(_titles)):
+                    new_titles.add_title(_titles[j])
+
+    def put(self, key, value):
+        if self.current_size > 0.7 * self.size:
+            self.rehash()
+        current = self.hash(key)
+        while self.keys[current] is not EMPTY:
+            if self.keys[current] == key:
+                self.values[current].add_title(value)
+                return
+            current = (current + 1) % self.size
+        self.current_size += 1
+        self.keys[current] = key
+        self.values[current] = table_keys_titles()
+        self.values[current].add_title(value)
+        
+    def add_title(self, author, title):
+        self.put(author, title)
+
+    def check_for_title(self, author, title):
+        titles = self.get(author)
+        if titles is None:
+            return False
+        return titles.check_for_title(title) 
+
+    def remove_title(self, author, title):
+        titles = self.get(author)
+        if titles is not None:
+            titles.remove_title(title)
+        
+
+    def get_sorted_titles_by_author(self, author):
+        current = self.hash(author)
+        while self.keys[current] is not EMPTY:
+            if self.keys[current] == author:
+                return self.values[current].get_all_sorted()
+            current = (current + 1) % self.size
+        return []
+        
+
+
+
+global library_table
 
 def init():
     """ Викликається 1 раз на початку виконання програми. """
-    pass
-
+    global library_table
+    library_table = table_keys_authors()
 
 def addBook(author, title):
     """ Додає книгу до бібліотеки.
     :param author: Автор книги
     :param title: Назва книги
     """
-    pass
-
+    global library_table
+    library_table.add_title(author, title)
 
 def find(author, title):
     """ Перевірає чи міститься задана книга у бібліотеці.
@@ -24,7 +194,8 @@ def find(author, title):
     :param title: Назва книги
     :return: True, якщо книга міститься у бібліотеці та False у іншому разі.
     """
-    return False
+    global library_table
+    return library_table.check_for_title(author, title)
 
 
 def delete(author, title):
@@ -32,7 +203,8 @@ def delete(author, title):
     :param author: Автор
     :param title: Назва книги
     """
-    pass
+    global library_table
+    library_table.remove_title(author, title)
 
 
 def findByAuthor(author):
@@ -41,5 +213,6 @@ def findByAuthor(author):
     :param author: Автор
     :return: Список книг заданого автора у алфавітному порядку.
     """
-    return []
+    global library_table
+    return library_table.get_sorted_titles_by_author(author)
 
